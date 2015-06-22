958e4353de1d9a26c7b00ec10ffb11d88bcbc481
Sergey Romanov <xxsmotur@gmail.com>
2015-06-22 20:37:01 +0500

    puppet for lle

--- manifold/knn.py
+++ manifold/knn.py
@@ -10,7 +10,6 @@ class KNN:
     def _dist(self, x1,x2, method='euclidean'):
         if method == 'euclidean':
             return euclidean(x1, x2)
-
     def fit(self, X):
         result = []
         length = X.shape[0]
@@ -25,6 +24,10 @@ class KNN:
             matrix[i] = array
         return matrix
 
+    def construct_weights(self, X):
+        length = X.shape[0]
+        for i in range(length):
+            yield sorted([(self._dist(X[i], f),j) for j,f in enumerate(X) if (X[i] != f).any()])[0:self.k]
 
 
 
---
+++ manifold/lle.py
@@ -0,0 +1,9 @@
+import numpy as np
+import knn
+
+class LLE(object):
+    def __init__(self):
+        pass
+
+    def fit(X,y):
+        kgraph = knn.KNN(5).fit(X)
